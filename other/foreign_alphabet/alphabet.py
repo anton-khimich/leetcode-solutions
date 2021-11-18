4176732c04b40da843f1ffe1f2fbacf2744289c8
Anton Khimich <anton.khimich@gmail.com>
2021-11-18 14:56:59 -0500

    Finish foreign alphabet problem with 2 solutions

--- other/foreign_alphabet/alphabet.py
+++ other/foreign_alphabet/alphabet.py
@@ -4,17 +4,54 @@
 # O(n+m) runtime
 # Ex: "d", "ro", "ri", "or" -> "droi"
 
-def longest_path(root: str, graph: dict[str, list[str]]) -> str:
-    path = root
-    if graph[root] == []:
-        return path
-    for node in graph[root]:
-        some_path = path + longest_path(node, graph)
-        if path < some_path:
-            path = some_path
-    return path
+from typing import List, Dict
+from queue import Queue
 
-def solution(words: list[str]) -> str:
+
+def top_sort_kahns(graph: Dict[str, str]) -> str:
+    ordering = ''
+    queue = Queue()
+    in_degree = {k:0 for k in graph}
+    for v in graph.values():
+        for node in v:
+            in_degree[node] += 1
+
+    for k, v in in_degree.items():
+        if v == 0:
+            queue.put(k)
+
+    while not queue.empty():
+        c = queue.get()
+        ordering += c
+        out_nodes = graph[c]
+        for node in out_nodes:
+            in_degree[node] -= 1
+            if in_degree[node] == 0:
+                queue.put(node)
+
+    return ordering
+
+def top_sort_dfs(graph: Dict[str, str]) -> str:
+    visited: List[str] = []
+
+    def dfs (root: str, graph: Dict[str, str]) -> str:
+        ordering = ''
+        if root not in visited:
+            visited.append(root)
+            for nodes in graph[root]:
+                for node in nodes:
+                    ordering += dfs(node, graph)
+            ordering += root
+        return ordering
+
+    reverse_order = ''
+    for c in graph:
+        reverse_order += dfs(c, graph)
+
+    return reverse_order[::-1]
+
+
+def solution(words: List[str]) -> str:
     alpha_map = {}
     for word in words:
         for c in word:
@@ -28,18 +65,10 @@ def solution(words: list[str]) -> str:
                 alpha_map[prev[j]].append(curr[j])
                 break
 
-    potential_roots = []
+    # return top_sort_dfs(alpha_map)
+    return top_sort_kahns(alpha_map)
 
-    # find the root
-    for key in alpha_map.keys():
-        potential_roots.append(key)
-
-    for val in alpha_map.values():
-        for letter in val:
-            if letter in potential_roots:
-                potential_roots.remove(letter)
-
-    # find longest path from root
-    return longest_path(potential_roots[0], alpha_map)
 
 print(solution(['wrt','wrf','er','ett','rftt'])) # wertf
+print(solution(['baa', 'abcd', 'abca', 'cab', 'cad'])) # bdac
+print(solution(['caa', 'aaa', 'aab'])) # cab
